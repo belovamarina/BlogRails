bb081ad4ea71fa00ebb17c8535e3f920a0969987
Marina Belova <belova.ma.ev@gmail.com>
2015-11-28 21:09:47 +0300

    Update model & test for Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,15 @@
 require "spec_helper"
 
 describe Comment do
-	it { should belong_to :article	}
-	it { should belong_to :user	}
+
+	describe 'associations' do
+		it { should belong_to :article	}
+		it { should belong_to :user	}
+	end
+
+	describe 'validations' do
+
+		it { should validate_length_of(:body).is_at_most(500) }
+
+	end
 end
