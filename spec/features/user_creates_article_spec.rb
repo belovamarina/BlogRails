c1cf6c7f30e2a4e2c107a5031e29e1855246c431
Marina Belova <belova.ma.ev@gmail.com>
2015-11-30 21:20:52 +0300

    Add user_create_article test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -2,12 +2,21 @@ require "spec_helper"
 
 feature "Article Creation" do
 
-  before(:all) do
+  before(:each) do
     sign_up
   end
 
     scenario "allows user to visit new article page" do
       visit new_article_path
-      expect(page).to have_content 'New article'
+      expect(page).to have_content I18n.t('articles.new_article')
     end  
+
+    scenario "allows a user to create article" do
+      visit new_article_path
+      fill_in :article_title, :with => 'capybaratesttitle'
+      fill_in :article_text, :with => 'capybaratesttext'
+      click_button "Save Article"
+
+      expect(page).to have_content I18n.t('articles.article_details')
+    end
 end
